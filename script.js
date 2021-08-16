17f6e3e7afa96d4db666db0468159fadfbf3244c
Flavio fernando da Silva <tchfernando@gmail.com>
2021-08-15 22:37:09 -0300

    set scores to 0 and hide dice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Selectin elements
+const score0Element = document.querySelector('#score--0');
+// getElementById is better when using a large number of data as it is a bit faster
+const score1Element = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+
+// Js will convert to string automatically
+// Starting conditions
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
